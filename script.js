5c0efc1808a3488ee4ff32341123b88e5f449a34
ramazangalip <ramazansaidgalip@gmail.com>
2023-07-24 13:54:28 +0300

    24.07.2023

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-// 1) sayinin tek mi çift mi olduğunu bulma
+//1) sayinin tek mi çift mi olduğunu bulma
 
-// sayi = 2
+sayi1 = console.log(prompt("Sayıyı giriniz: "))
 
-// if(sayi == 1){
-//     console.log("Tek sayi")
-
-// }
-// if(sayi == 3){
-//     console.log("Tek Sayi")
-
-// }
-// if(sayi == 5){
-//     console.log("Tek Sayi")
-// }
-// if(sayi == 7){
-//     console.log("Tek Sayi")
-// }
-// if(sayi== 9){
-//     console.log("Tek sayi")
-// }
-// if(sayi == 2){
-//     console.log("Çift Sayi")
-// }
-// if(sayi == 4){
-//     console.log("Çİft Sayi")
-// }
-// if(sayi == 6){
-//     console.log("Çift Sayi")
-// }
-// if(sayi == 8){
-//     console.log("Çift sayi")
-// }
-// if(sayi == 10){
-//     console.log("Çift Sayi")
-// }
+if(sayi1 %2 == 0){
+    console.log("Sayi çift");
+}
+if(sayi1%2 != NaN){
+    console.log("Sayi Tek");
+}
 
 // 2) Girilen sayı 4, 5 ve 6 sayılarından en az birine bölünüyorsa “Sayı Uygun”, aksi halde “Sayı Uygun Değil” mesajı veren programı yazınız
 // let sayi = console.log(prompt("sayiyi giriniz"))
@@ -46,12 +20,12 @@
 // }
 
 // 3)  
-let sayi = console.log(prompt("sayiyi giriniz"))
+// let sayi = console.log(prompt("sayiyi giriniz"))
 
-if(sayi<100 && sayi>0 && sayi%2 == 0){
-    console.log("sayi uygun");
+// if(sayi<100 && sayi>0 && sayi%2 == 0){
+//     console.log("sayi uygun");
     
-}
-else{
-    console.log("sayi uygun değil");
-}
+// }
+// else{
+//     console.log("sayi uygun değil");
+// }
